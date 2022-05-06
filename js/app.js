99ceafee62693ac3ec5b7c33f6e445975ff79d86
Sudzuya <79112652+Sudzuya@users.noreply.github.com>
2022-05-06 09:09:45 +0300

    .

--- js/app.js
+++ js/app.js
@@ -3726,7 +3726,7 @@
         document.querySelector(".spollers-publications").classList.toggle("active");
         if (document.querySelector(".spollers-publications").classList.contains("active")) {
             console.log("da");
-            document.querySelector(".publications .spollers__body").style.height = "300px";
+            document.querySelector(".publications .spollers__body").style.height = "3000px";
         } else document.querySelector(".publications .spollers__body").style.height = "80px";
     }));
     window["FLS"] = true;
